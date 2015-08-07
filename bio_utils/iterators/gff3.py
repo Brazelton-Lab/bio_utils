4ad3e90e4c7950116c325eaca89cd9c4283614c4
Alex Jay Hyer <TheOneHyer@linux-3tt7.site>
2015-08-07 12:03:37 -0600

    Fixed gff3_iter to stop reading before FASTA entries

--- bio_utils/iterators/gff3.py
+++ bio_utils/iterators/gff3.py
@@ -2,33 +2,42 @@
 
 '''Screed-esque iterator for GFF3 files (not headers)'''
 
-__version__ = '1.0.0.0'
+__version__ = '1.2.1.0'
 
-def gff3_iter(handle):
+def gff3_iter(handle, prokka=False):
     '''
     Iterator over the given GFF3 file handle, returning records. handle
-    is a handle to a file opened for reading
+    is a handle to a file opened for reading. The PROKKA option
+    further parses the attributes by the GFF3 output of PROKKA 1.12-beta
     '''
-        
+
     for line in handle:
-        
+
         if line.startswith('##'):
             continue
 
-        if line.startswith('>'):
+        if line == '##FASTA\n':
             break
 
         line = line.strip()
         data = {}
-        splitLine = line.split('\t')
-        data['seqid'] = splitLine[0]
-        data['source'] = splitLine[1]
-        data['type'] = splitLine[2]
-        data['start'] = splitLine[3]
-        data['end'] = splitLine[4]
-        data['score'] = splitLine[5]
-        data['strand'] = splitLine[6]
-        data['phase'] = splitLine[7]
-        data['attributes'] = splitLine[8]
-        
+        split_line = line.split('\t')
+        data['seqid'] = split_line[0]
+        data['source'] = split_line[1]
+        data['type'] = split_line[2]
+        data['start'] = split_line[3]
+        data['end'] = split_line[4]
+        data['score'] = split_line[5]
+        data['strand'] = split_line[6]
+        data['phase'] = split_line[7]
+        data['attributes'] = split_line[8]
+
+        if prokka:
+            attributes = data['attributes'].split(';')
+            for attribute in attributes:
+                split_attribute = attribute.split('=')
+                key = split_attribute[0]
+                value = split_attribute[-1]
+                data[key] = value
+
         yield data
