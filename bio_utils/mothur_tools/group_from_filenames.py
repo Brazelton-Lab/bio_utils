85da091f774ae483df8909ebc44dae42f876ef56
Alex Jay Hyer <TheOneHyer@linux-3tt7.site>
2015-08-27 13:25:16 -0600

    Added convert_count_to_shared.py
    convert_count_to_shared added to mothur_tools. This script
    effectively bypasses OTU generation in MOTHUR whiel allowing
    downstream analysis.

--- bio_utils/mothur_tools/group_from_filenames.py
+++ bio_utils/mothur_tools/group_from_filenames.py
@@ -1,1 +1,10 @@
-__author__ = 'TheOneHyer'
+#! /usr/bin/env python
+
+from __future__ import print_function
+
+import argparse
+from bio_utils.file_tools.file_check import IOChecker
+import os
+import sys
+import textwrap
+
