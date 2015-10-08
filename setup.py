2b7ece1fd2e71e26c28390cb128555c1d339a8c6
Alex Jay Hyer <TheOneHyer@linux-3tt7.site>
2015-10-08 11:01:53 -0600

    Fixing merging issues

--- setup.py
+++ setup.py
@@ -3,11 +3,7 @@
 from setuptools import setup
 
 setup(name='bio_utils',
-<<<<<<< HEAD
-      version='0.7.2.0',
-=======
-      version='0.7.3.0',
->>>>>>> a985323bcd4c291bff915d6ded00cee5e83a9bb0
+      version='0.7.4.0',
       description='importable functions often used by bioinformatic scripts',
       classifiers=[
           'Development Status :: 6 - Mature',
@@ -23,11 +19,7 @@ setup(name='bio_utils',
       keywords='bioinformatics iterators verifiers verify iterate utilities',
       url='https://github.com/Brazelton-Lab/bio_utils/',
       download_url='https://github.com/Brazelton-Lab/metameta/tarball/' \
-<<<<<<< HEAD
-                   + '0.7.2.0',
-=======
-                   + '0.7.3.0',
->>>>>>> a985323bcd4c291bff915d6ded00cee5e83a9bb0
+                   + '0.7.4.0',
       author='Alex Hyer',
       author_email='theonehyer@gmail.com',
       license='GPL',
